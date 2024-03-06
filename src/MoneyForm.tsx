47261d085dd9c4fd1e791414da811a1aa48b99cf
Sinchana P <sinchanap@ivoyant.com>
2024-03-06 07:31:28 +0530

    toJS

--- src/MoneyForm.tsx
+++ src/MoneyForm.tsx
@@ -1,31 +1,38 @@
-import { action, computed, observable } from 'mobx';
+import { action, computed, observable, toJS } from 'mobx';
 import { observer } from 'mobx-react-lite'
 import React, { useState } from 'react'
 
 type FormState = {
-    total: number;
+    // total: number;
     years: number;
     salary: number;
 }
 
 const formState: FormState = observable({
-    total: 0,
+    // total: 0,
     years: 0,
     salary: 0
 })
 
-const calculateTotal = action( (formState: FormState) => {
-    console.log(formState.total)
-    formState.total = formState.years * formState.salary;
-    console.log(formState.total)
-})
+// const calculateTotal = action( (formState: FormState) => {
+//     console.log(formState.total)
+//     formState.total = formState.years * formState.salary;
+//     console.log(formState.total)
+// })
 
 const MoneyForm = () => {
+
+    const totalValue = computed(() => formState.salary * formState.years)
+
+    // Utility Function:
+    // toJS: it takes a observable & it converts into non-observable value.
+    console.log(toJS(totalValue))
 
   return (
     <div style={{ display: 'flex', flexDirection: "column" }}>
         <h1 style={{ marginBottom: 0 }}>Money Talks</h1>
-        <p>Total: {formState.total}</p>
+        {/* <p>Total: {totalValue.get()}</p> */}
+        <>Total: {toJS(totalValue)}</>
         <input 
         type='number' 
         placeholder='Years...' 
@@ -40,9 +47,10 @@ const MoneyForm = () => {
             onChange={action((e) => {
                 formState.salary = Number(e.target.value)
             })} />
-        <button type='button' onClick={() => calculateTotal(formState)}>
+
+        {/* <button type='button' onClick={() => calculateTotal(formState)}>
             calculate Total
-        </button>
+        </button> */}
     </div>
   )
 }
